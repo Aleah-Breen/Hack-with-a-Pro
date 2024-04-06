6e4d60a96f89f5bb23da22e736485b27de5eb336
Huy Nguyen <haderachi@g.ucla.edu>
2024-04-06 15:15:58 -0700

    Added JS search file

---
+++ search.js
@@ -0,0 +1,17 @@
+function initMap() {
+    // Diddy Riese data (for now)
+    const loc_Diddy_Riese = new google.maps.LatLng(34.063050026926234, -118.44685020759215);
+    var mapProp= {
+      center: loc_Diddy_Riese,
+      zoom: 50,
+    };
+  
+    // Define a map variable
+    var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+  
+    // Define a Marker variable
+    var marker = new google.maps.Marker({position: loc_Diddy_Riese});
+  
+    marker.setMap(map); 
+}
+  
